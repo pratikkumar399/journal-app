1dafb8b97bb47de2dd0652b8ea28ce5b719e4614
raipratik <pratikkumar399@gmail.com>
2023-06-18 00:52:18 +0530

    changes to env

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'Pratik@123',
-        database: 'journal_app',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
 });
 
